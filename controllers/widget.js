3f230742dfc55ce2a433b0bea05b0a2a1101c6f7
GeekKingCloud <idhowarth7@gmail.com>
2015-08-07 23:17:51 -0500

    Spelling error fixed

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,16 +1,16 @@
 function setLeftLabel( options ) {
     $.leftButtonLabel.text = options.text || "No Text Set";
-    option.eventFunction ? $.leftButtonLabel.addEventListener( "click" , options.eventFunction ) : 0;
+    options.eventFunction ? $.leftButtonLabel.addEventListener( "click" , options.eventFunction ) : 0;
 }
 
 function setRightLabel( options ) {
     $.rightButtonLabel.text = options.text || "No Text Set";
-    option.eventFunction ? $.rightButtonLabel.addEventListener( "click" , options.eventFunction ) : 0;
+    options.eventFunction ? $.rightButtonLabel.addEventListener( "click" , options.eventFunction ) : 0;
 }
 
 function setTitleLabel( options ) {
     $.titleLabel.text = options.text || "No Text Set";
-    option.eventFunction ? $.titleLabel.addEventListener( "click" , options.eventFunction ) : 0;
+    options.eventFunction ? $.titleLabel.addEventListener( "click" , options.eventFunction ) : 0;
 }
 
 exports.setLeftLabel  = setLeftLabel;
