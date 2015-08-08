d706065f6b37289e0c70f7852b0a1418b5d20198
GeekKingCloud <idhowarth7@gmail.com>
2015-08-07 23:12:42 -0500

    Tertiary checks incase we don't want to add an eventListener
    Exactly taht ^

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,16 +1,16 @@
 function setLeftLabel( options ) {
     $.leftButtonLabel.text = options.text || "No Text Set";
-    $.leftButtonLabel.addEventListener( "click" , options.eventFunction );
+    option.eventFunction ? $.leftButtonLabel.addEventListener( "click" , options.eventFunction ) : 0;
 }
 
 function setRightLabel( options ) {
     $.rightButtonLabel.text = options.text || "No Text Set";
-    $.rightButtonLabel.addEventListener( "click" , options.eventFunction );
+    option.eventFunction ? $.rightButtonLabel.addEventListener( "click" , options.eventFunction ) : 0;
 }
 
 function setTitleLabel( options ) {
     $.titleLabel.text = options.text || "No Text Set";
-    $.titleLabel.addEventListener( "click" , options.eventFunction );
+    option.eventFunction ? $.titleLabel.addEventListener( "click" , options.eventFunction ) : 0;
 }
 
 exports.setLeftLabel  = setLeftLabel;
